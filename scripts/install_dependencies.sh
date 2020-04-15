c2171746f41b7415c1a02b301784649b4d921770
lidia-saf <lidiia.safiullina@yandex.ru>
2020-04-15 01:04:32 -0700

    One fix to rule them all.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,7 +2,7 @@
 # update yum just in case
 yum update -y
 # get node into yum
-curl --silent --location https://rpm.nodesource.com/setup_8.x| bash -
+curl --silent --location https://rpm.nodesource.com/setup_10.x| bash -
 # install node and npm in one line
 yum install -y nodejs
 # install pm2 to restart node app
