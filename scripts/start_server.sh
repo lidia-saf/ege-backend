2e1753303a1a697dc965b1422a352043e5756784
lidia-saf <lidiia.safiullina@yandex.ru>
2020-04-15 00:07:55 -0700

    Fixing bug in script.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,6 @@
 # sudo chmod 755 /var/www/server.js # optional
 # this will restart app/server on instance reboot
 crontab -l | { cat; echo "@reboot pm2 start \"npm run startProduction\" -i 0 --name \"ege-backend\""; } | crontab -
+sudo pm2 stop ege-backend
 # actually start the server
-pm2 start "npm run startProduction" --name "ege-backend"
+sudo pm2 start "npm run startProduction" --name "ege-backend"
